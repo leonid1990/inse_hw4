50aa9586fb19d3a2f1b3267570eca1582c826282
unknown <leonid1990@gmail.com>
2019-05-03 00:09:17 +0300

    added login and bootstrap modal

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.set("view engine", "ejs");
 const port = 3000;
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function(req, res) {
+  res.render("pages/index.ejs");
+});
 
 app.listen(port, () => console.log(`App is listenting on port ${port}`));
