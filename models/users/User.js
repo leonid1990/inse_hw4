e24ed568cf94222232a010bdcd638317c868a8fc
unknown <leonid1990@gmail.com>
2019-05-10 04:18:45 +0300

    fixed User model

--- models/users/User.js
+++ models/users/User.js
@@ -1,8 +1,39 @@
-function User(id, typeOfUser, userName, password) {
+exports.User = function(id, typeOfUser, userName, password) {
   this.id = id;
   this.typeOfUser = typeOfUser;
   this.userName = userName;
   this.password = password;
+};
+
+var allUsersData = require("../data").users;
+
+function serializeUser(singleUserObj) {
+  return JSON.stringify(singleUserObj);
 }
 
-module.exports = User;
+function deSerializeUser(singleUserData) {
+  var userObj = new User(
+    singleUserData.id,
+    singleUserData.typeOfUser,
+    singleUserData.userName,
+    singleUserData.password
+  );
+
+  return userObj;
+}
+exports.getUserById = function(id) {
+  return deSerializeUser(allUsersData.find(user => user.id === id));
+};
+exports.getUserByName = function(userName) {
+  return deSerializeUser(allUsersData.find(user => user.userName === userName));
+};
+exports.getAllUsers = function() {
+  return allUsersData.map(deSerializeUser(singleUserData));
+};
+exports.createUser = function(user) {
+  if (getUserById(user.id)) {
+    throw "User already exists";
+  } else {
+    allUsersData.push(serializeUser(user));
+  }
+};
