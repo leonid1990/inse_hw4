b0e645a4d3d2e86b64bdf2a7154533333b74c4c4
unknown <leonid1990@gmail.com>
2019-05-26 18:28:01 +0300

    basic login functionality works

--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
+const usersController = require("../controllers/users");
+
 router.post("/login", function(req, res) {
-  console.log(req.body);
-  res.sendStatus(200);
+  var isValid = false;
+  try {
+    isValid = usersController.validateUser(
+      req.body.userName,
+      req.body.password
+    );
+  } catch (error) {
+    console.log(error);
+  }
+  if (isValid) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 module.exports = router;
