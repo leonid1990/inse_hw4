04001f592b68a04a0899bca9c7074569f6fa2e1f
unknown <leonid1990@gmail.com>
2019-06-07 10:44:10 +0300

    added flowers tab

--- public/scripts/update_view.js
+++ public/scripts/update_view.js
@@ -1,10 +1,35 @@
 function updateView(userData) {
-      $("#btn").text("Logout");
-      document.getElementById("btn").removeAttribute("data-target");
-      $("#btn").bind("click", function() {
-        window.location.assign("/");
-      });
+  $("#btn").text("Logout");
+  document.getElementById("btn").removeAttribute("data-target");
+  $("#btn").bind("click", function() {
+    window.location.assign("/");
+  });
 
-      var para = document.createElement("p");
-      var para = document.createElement("p");
-    }
+  createFlowersEntry();
+}
+
+function createFlowersEntry() {
+  /* add flowers tab */
+  var tabFlowers_a = document.createElement("a");
+  tabFlowers_a.setAttribute("data-toggle", "tab");
+  tabFlowers_a.setAttribute("href", "#flowers");
+  var tabFlowers_a_txt = document.createTextNode("Flowers Catalog");
+  tabFlowers_a.appendChild(tabFlowers_a_txt);
+  var tabFlowers = document.createElement("li");
+  tabFlowers.setAttribute("id", "flowersTab");
+  tabFlowers.appendChild(tabFlowers_a);
+  var tabs = document.getElementsByClassName("nav-tabs")[0];
+  var aboutTab = document.getElementById("aboutTab");
+  tabs.insertBefore(tabFlowers, aboutTab);
+  /* add flowers div */
+
+  var divFlowers = document.createElement("div");
+  divFlowers.setAttribute("id", "flowers");
+  divFlowers.setAttribute("class", "tab-pane fade");
+  var divFlowers_content = document.createTextNode("Flowers Content");
+  divFlowers.appendChild(divFlowers_content);
+
+  var tabContent = document.getElementsByClassName("tab-content")[0];
+  var about = document.getElementById("about");
+  tabContent.insertBefore(divFlowers, about);
+}
